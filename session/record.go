8ef497ae74d5fd7f4106221f89687c094e7f7c9a
W-xuan <951254767@qq.com>
2022-08-31 07:41:38 +0800

    update

--- session/record.go
+++ session/record.go
@@ -9,6 +9,7 @@ import (
 func (s *Session) Insert(values ...interface{}) (int64, error) {
 	recordValues := make([]interface{}, 0)
 	for _, value := range values {
+		s.CallMethod(BeforeInsert, value)
 		table := s.Model(value).RefTable()
 		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
 		recordValues = append(recordValues, table.RecordValues(value))
@@ -20,13 +21,16 @@ func (s *Session) Insert(values ...interface{}) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
+	s.CallMethod(AfterInsert, nil)
 	return result.RowsAffected()
 }
 
 func (s *Session) Find(values interface{}) error {
+	s.CallMethod(BeforeQuery, nil)
 	destSlice := reflect.Indirect(reflect.ValueOf(values))
 	destType := destSlice.Type().Elem()
 	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable()
+
 	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)
 	sql, vars := s.clause.Build(clause.SELECT, clause.WHERE, clause.ORDERBY, clause.LIMIT)
 	rows, err := s.Raw(sql, vars...).QueryRows()
@@ -43,47 +47,23 @@ func (s *Session) Find(values interface{}) error {
 		if err := rows.Scan(values...); err != nil {
 			return err
 		}
+		s.CallMethod(AfterQuery, dest.Addr().Interface())
 		destSlice.Set(reflect.Append(destSlice, dest))
 	}
 	return rows.Close()
 }
 
-func (s *Session) Update(kv ...interface{}) (int64, error) {
-	m, ok := kv[0].(map[string]interface{})
-	if !ok {
-		m = make(map[string]interface{})
-		for i := 0; i < len(kv); i += 2 {
-			m[kv[i].(string)] = kv[i+1]
-		}
+func (s *Session) First(value interface{}) error {
+	dest := reflect.Indirect(reflect.ValueOf(value))
+	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
+	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
+		return err
 	}
-	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
-	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
-	result, err := s.Raw(sql, vars...).Exec()
-	if err != nil {
-		return 0, err
+	if destSlice.Len() == 0 {
+		return errors.New("NOT FOUND")
 	}
-	return result.RowsAffected()
-}
-
-func (s *Session) Delete() (int64, error) {
-	s.clause.Set(clause.DELETE, s.RefTable().Name)
-	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
-	result, err := s.Raw(sql, vars...).Exec()
-	if err != nil {
-		return 0, err
-	}
-	return result.RowsAffected()
-}
-
-func (s *Session) Count() (int64, error) {
-	s.clause.Set(clause.COUNT, s.RefTable().Name)
-	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
-	row := s.Raw(sql, vars...).QueryRow()
-	var tmp int64
-	if err := row.Scan(&tmp); err != nil {
-		return 0, err
-	}
-	return tmp, nil
+	dest.Set(destSlice.Index(0))
+	return nil
 }
 
 func (s *Session) Limit(num int) *Session {
@@ -102,15 +82,44 @@ func (s *Session) OrderBy(desc string) *Session {
 	return s
 }
 
-func (s *Session) First(value interface{}) error {
-	dest := reflect.Indirect(reflect.ValueOf(value))
-	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
-	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
-		return err
+func (s *Session) Update(kv ...interface{}) (int64, error) {
+	s.CallMethod(BeforeUpdate, nil)
+	m, ok := kv[0].(map[string]interface{})
+	if !ok {
+		m = make(map[string]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
 	}
-	if destSlice.Len() == 0 {
-		return errors.New("NOT FOUND")
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
 	}
-	dest.Set(destSlice.Index(0))
-	return nil
+	s.CallMethod(AfterUpdate, nil)
+	return result.RowsAffected()
+}
+
+func (s *Session) Delete() (int64, error) {
+	s.CallMethod(BeforeDelete, nil)
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	s.CallMethod(AfterDelete, nil)
+	return result.RowsAffected()
+}
+
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	row := s.Raw(sql, vars...).QueryRow()
+	var tmp int64
+	if err := row.Scan(&tmp); err != nil {
+		return 0, err
+	}
+	return tmp, nil
 }
--- session/record_test.go
+++ session/record_test.go
@@ -20,12 +20,58 @@ func testRecordInit(t *testing.T) *Session {
 	return s
 }
 
+func TestSession_Insert(t *testing.T) {
+	s := testRecordInit(t)
+	affected, err := s.Insert(user3)
+	if err != nil || affected != 1 {
+		t.Fatal("failed to create record")
+	}
+}
+
+func TestSession_Find(t *testing.T) {
+	s := testRecordInit(t)
+	var users []User
+	if err := s.Find(&users); err != nil || len(users) != 2 {
+		t.Fatal("failed to query all")
+	}
+}
+
+func TestSession_First(t *testing.T) {
+	s := testRecordInit(t)
+	u := &User{}
+	err := s.First(u)
+	if err != nil || u.Name != "Tom" || u.Age != 18 {
+		t.Fatal("failed to query first")
+	}
+}
+
 func TestSession_Limit(t *testing.T) {
 	s := testRecordInit(t)
 	var users []User
 	err := s.Limit(1).Find(&users)
 	if err != nil || len(users) != 1 {
 		t.Fatal("failed to query with limit condition")
+	}
+}
+
+func TestSession_Where(t *testing.T) {
+	s := testRecordInit(t)
+	var users []User
+	_, err1 := s.Insert(user3)
+	err2 := s.Where("Age = ?", 25).Find(&users)
+
+	if err1 != nil || err2 != nil || len(users) != 2 {
+		t.Fatal("failed to query with where condition")
+	}
+}
+
+func TestSession_OrderBy(t *testing.T) {
+	s := testRecordInit(t)
+	u := &User{}
+	err := s.OrderBy("Age DESC").First(u)
+
+	if err != nil || u.Age != 25 {
+		t.Fatal("failed to query with order by condition")
 	}
 }
 
@@ -47,12 +93,5 @@ func TestSession_DeleteAndCount(t *testing.T) {
 
 	if affected != 1 || count != 1 {
 		t.Fatal("failed to delete or count")
-	}
-}
-func TestSession_Find(t *testing.T) {
-	s := testRecordInit(t)
-	var users []User
-	if err := s.Find(&users); err != nil || len(users) != 2 {
-		t.Fatal("failed to query all")
 	}
 }
